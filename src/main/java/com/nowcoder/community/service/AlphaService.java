ff1be7f7219333e3d42dcad404210cd17d4fa567
香蕉你个比巴卜 <han_jlu@126.com>
2022-04-29 12:28:33 +0800

    事务管理

--- src/main/java/com/nowcoder/community/service/AlphaService.java
+++ src/main/java/com/nowcoder/community/service/AlphaService.java
@@ -59,9 +59,13 @@ public class AlphaService {
         return alphaDao.select();
     }
 
-    // REQUIRED: 支持当前事务(外部事务),如果不存在则创建新事务.
-    // REQUIRES_NEW: 创建一个新事务,并且暂停当前事务(外部事务).
-    // NESTED: 如果当前存在事务(外部事务),则嵌套在该事务中执行(独立的提交和回滚),否则就会REQUIRED一样.
+    /**
+     * REQUIRED: 支持当前事务(外部事务),如果不存在则创建新事务.
+     * REQUIRES_NEW: 创建一个新事务,并且暂停当前事务(外部事务).
+     * NESTED: 如果当前存在事务(外部事务),则嵌套在该事务中执行(独立的提交和回滚),否则就会REQUIRED一样.
+     *
+     * @return  通用，测试返回字符串“ok”
+     */
     @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
     public Object save1() {
         // 新增用户
@@ -91,31 +95,28 @@ public class AlphaService {
         transactionTemplate.setIsolationLevel(TransactionDefinition.ISOLATION_READ_COMMITTED);
         transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
 
-        return transactionTemplate.execute(new TransactionCallback<Object>() {
-            @Override
-            public Object doInTransaction(TransactionStatus status) {
-                // 新增用户
-                User user = new User();
-                user.setUserName("beta");
-                user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
-                user.setPassWord(CommunityUtil.md5("123" + user.getSalt()));
-                user.setEmail("beta@qq.com");
-                user.setHeaderUrl("http://image.nowcoder.com/head/999t.png");
-                user.setCreateTime(new Date());
-                userMapper.insertUser(user);
+        return transactionTemplate.execute((TransactionCallback<Object>) status -> {
+            // 新增用户
+            User user = new User();
+            user.setUserName("beta");
+            user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+            user.setPassWord(CommunityUtil.md5("123" + user.getSalt()));
+            user.setEmail("beta@qq.com");
+            user.setHeaderUrl("http://image.nowcoder.com/head/999t.png");
+            user.setCreateTime(new Date());
+            userMapper.insertUser(user);
 
-                // 新增帖子
-                DiscussPost post = new DiscussPost();
-                post.setUserId(user.getId());
-                post.setTitle("你好");
-                post.setContent("我是新人!");
-                post.setCreateTime(new Date());
-                discussPostMapper.insertDiscussPost(post);
+            // 新增帖子
+            DiscussPost post = new DiscussPost();
+            post.setUserId(user.getId());
+            post.setTitle("你好");
+            post.setContent("我是新人!");
+            post.setCreateTime(new Date());
+            discussPostMapper.insertDiscussPost(post);
 
-                Integer.valueOf("abc");
+            Integer.valueOf("abc");
 
-                return "ok";
-            }
+            return "ok";
         });
     }
 
