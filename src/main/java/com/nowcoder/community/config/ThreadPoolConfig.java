b4360f431dddbd477a813dd4f631fc5729c77c89
香蕉你个比巴卜 <han_jlu@126.com>
2022-05-24 20:02:41 +0800

    任务执行和调度

---
+++ src/main/java/com/nowcoder/community/config/ThreadPoolConfig.java
@@ -0,0 +1,18 @@
+package com.nowcoder.community.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author HAN
+ * @date 2022/5/24 16:47
+ */
+@Configuration
+@EnableScheduling
+@EnableAsync
+public class ThreadPoolConfig {
+
+
+
+}
